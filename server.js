f0d961d080cc75f4b6ab3c51f266267fe9a1945a
mattdamon108 <woonki.moon@gmail.com>
2019-07-16 01:28:13 +0900

    add static test server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+
+const app = express();
+const config = require("./webpack.config.js");
+const compiler = webpack(config);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  })
+);
+
+app.use(require("webpack-hot-middleware")(compiler));
+
+app.listen(3000, function() {
+  console.log("Webpack dev server is listening on port 3000");
+});
